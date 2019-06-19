6ebf24ac3db3ffd1dd9226169c6b4e7521e73cca
Alex Strive <3083076-alexstrive@users.noreply.gitlab.com>
2019-06-19 18:22:30 +0300

    Remove redundant import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import AllProviders from './AllProviders';
 
-import PoolListPage from './pages/PoolListPage';
 import PoolPage from './pages/PoolPage';
 
 const App = () => (
