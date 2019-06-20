f0e7fec224de72a28d386f2958a14cb91502838e
Denis Shtefan <den.shtefan@gmail.com>
2019-06-20 14:42:51 +0300

    Implement button stretching

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -4,7 +4,11 @@ import PropTypes from "prop-types"
 import { Button } from "@vkontakte/vkui"
 
 const NextButton = ({ onClick }) => {
-  return <Button onClick={onClick}>Далее</Button>
+  return (
+    <Button size="l" stretched onClick={onClick}>
+      Далее
+    </Button>
+  )
 }
 
 NextButton.propTypes = {
