66394d8622e3da2ea38362897d81721dee3fbe75
Alex Strive <3083076-alexstrive@users.noreply.gitlab.com>
2019-06-20 12:32:44 +0300

    Extract into external module mock data

--- src/pages/PoolPage.js
+++ src/pages/PoolPage.js
@@ -7,65 +7,9 @@ import SubmitButton from "../components/SubmitButton"
 import BackButton from "../components/BackButton"
 import Question from "../components/questions/Question"
 
-const mockPoolList = [
-  {
-    id: "12XJWWr-Z8gkRdxrkwoU8CYg1h8GqWv3OJh-AOLzpyyQ",
-
-    title: "Заголовок опроса",
-    description: "Описание опроса",
-    companyId: "",
-    editorEmails: ["stevenschmatz@gmail.com"],
-    confirmationMessage: "Thanks for submitting your contact info!",
-
-    questions: [
-      {
-        type: "TEXTAREA",
-        helpText: "Little description",
-        placeholder: "",
-        title: "Name",
-        id: 1633920210,
-        isRequired: false,
-      },
-      {
-        type: "MULTIPLE_CHOICE",
-        helpText: "",
-        id: 1770822543,
-        title: "How much do you like choices?",
-        isRequired: false,
-        hasOtherOption: true,
-        placeholder: "",
-        options: ["Option 1", "Option 2"],
-      },
-      {
-        type: "CHECKBOX",
-        helpText: "Description",
-        id: 1846923513,
-        title: "How much do you like checkboxes?",
-        isRequired: false,
-        hasOtherOption: true,
-        placeholder: "",
-        options: ["Option 1 Check", "Option 2 Check"],
-      },
-      {
-        type: "DROPDOWN",
-        helpText: "",
-        id: 449887830,
-        title: "How much do you like dropdowns?",
-        isRequired: false,
-        placeholder: "",
-        options: [
-          "Option 1 Dropdown",
-          "Option 2 Dropdown",
-          "Option 3 Dropdown",
-        ],
-      },
-    ],
-  },
-]
-
 const PoolPage = ({ location }) => {
   const poolId = location.pathname.slice(1)
-  const poolData = mockPoolList.filter((pool) => pool.id === poolId)[0]
+  const poolData = getSurvey(poolId)
 
   const [activePanel, setActivePanel] = useState(0)
   const [userAnswers, setUserAnswers] = useState([])
