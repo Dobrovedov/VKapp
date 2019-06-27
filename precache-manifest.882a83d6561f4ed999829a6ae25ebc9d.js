3ad4c5a374f671eaa1d8d24bf8c9c388f44d2bb3
boris <boriskakrut@gmail.com>
2019-06-27 18:18:00 +0300

    Create github pages

---
+++ precache-manifest.882a83d6561f4ed999829a6ae25ebc9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912513a90307f260479774aee46a99bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "847bb11bd68addc83207",
+    "url": "/static/css/2.7df38a7a.chunk.css"
+  },
+  {
+    "revision": "847bb11bd68addc83207",
+    "url": "/static/js/2.28ee28be.chunk.js"
+  },
+  {
+    "revision": "e3b2988d97cac352d2a8",
+    "url": "/static/js/main.c3a798a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import SurveyPage from "./pages/SurveyPage"
-
-const App = () => {
-  return <SurveyPage />
-}
-
-export default App
