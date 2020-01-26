1bb109d0e881bf8b3543c4146e47aaa1a4f1e78e
Dobrovedov <alexnemerovskii@gmail.com>
2020-01-26 20:37:41 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 
 export const getSurvey = (id) =>
-  axios.get(`https://foodtechmoneymaker.herokuapp.com/surveys/${id}`, {
+  axios.get(`https://survey-app-itmo.herokuapp.com/surveys/${id}`, {
     mode: "no-cors",
   })
 
 export const sendAnswers = (id, response, user) =>
   axios.post(
-    `https://foodtechmoneymaker.herokuapp.com/surveys/${id}/responses`,
+    `https://survey-app-itmo.herokuapp.com/surveys/${id}/responses`,
     { ...response, vkUser: user },
     {
       mode: "no-cors",
@@ -16,7 +16,7 @@ export const sendAnswers = (id, response, user) =>
 
 export const sendChangedAnswers = (surveyId, responseId, response, user) =>
   axios.put(
-    `https://foodtechmoneymaker.herokuapp.com/surveys/${surveyId}/responses/${responseId}`,
+    `https://survey-app-itmo.herokuapp.com/surveys/${surveyId}/responses/${responseId}`,
     { ...response, vkUser: user },
     {
       mode: "no-cors",
